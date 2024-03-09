f2d18f69fb6dd0a625befb777f16059a9d88301e
Maxim Rukoleev <maxprolife@icloud.com>
2024-03-09 22:26:45 +0300

    url name, namespace

--- mysite/ecommerce/urls.py
+++ mysite/ecommerce/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import index, contacts, indexProduct
+
+app_name="ecommerce"
+
 urlpatterns = [
     #  http://127.0.0.1:8000/
     path('', index),
 
     #  http://127.0.0.1:8000/1/
-    path('<int:product_id>/', indexProduct),
+    path('<int:product_id>/', indexProduct, name="detail"),
 
     #  http://127.0.0.1:8000/contacts/
     path('contacts/', contacts),

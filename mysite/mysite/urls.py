d4aa4624fe92a22f7759fa70dc18b31714d188d7
Maxim Rukoleev <maxprolife@icloud.com>
2024-03-12 14:49:10 +0300

    added image field in models

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # http://127.0.0.1:8000/admin/
@@ -8,3 +10,5 @@ urlpatterns = [
     #  http://127.0.0.1:8000/
     path("", include("ecommerce.urls", namespace="ecommerce"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

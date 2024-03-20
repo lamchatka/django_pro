779f42f4fe5f0540297894f79c68a0b23aad3f30
Maxim Rukoleev <maxprolife@icloud.com>
2024-03-20 21:13:21 +0300

    form stylization

--- mysite/tailwind.config.js
+++ mysite/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./ecommerce/**/*.{html,js}"],
+  content: ["./ecommerce/**/*.{html,js}", "./users/**/*.{html,js}",],
   theme: {
     extend: {},
   },
